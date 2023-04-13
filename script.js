748aa5e262228f0d023d870698f3dc33daa516f7
zatsarna <lyudazats@gmail.com>
2023-04-12 21:32:33 -0400

    codewars2

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function feast(beast, dish) {
     beast.charAt(beast.length - 1) === dish.charAt(dish.length - 1)
   );
 }
+
+/*Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. */
+const countSheep = function (num) {
+  let str = "";
+  for (let i = 1; i <= num; i++) {
+    str += `${i} sheep...`;
+  }
+  return str;
+};
+console.log(countSheep(3));
