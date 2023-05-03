e55e2789724609914f7364d4b14a9be73248549a
zatsarna <lyudazats@gmail.com>
2023-05-03 08:53:49 -0400

    Array methods map / reduce

--- script.js
+++ script.js
@@ -86,3 +86,37 @@ function rowSumOddNumbers(n) {
   }
   return sum;
 }
+/*Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even" */
+function oddOrEven(array) {
+  return array.reduce((sum, item) => sum + item, 0) % 2 === 0 ? "even" : "odd";
+}
+/*Enjoying your holiday, you head out on a scuba diving trip!
+
+Disaster!! The boat has caught fire!!
+
+You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+*/
+function fireFight(s) {
+  return s
+    .split(" ")
+    .map((el) => {
+      return el === "Fire" ? "~~" : el;
+    })
+    .join(" ");
+}
+console.log(fireFight("Mast Deck Engine Water Fire"));
