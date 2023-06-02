a31135713c5399243bdf6f478ffe2a7c6bc0dd11
zatsarna <lyudazats@gmail.com>
2023-06-02 12:23:20 -0400

    method sort

--- script.js
+++ script.js
@@ -270,3 +270,16 @@ const sumSquareEvenRootOdd = (ns) => {
   }
   return Number(newArr.reduce((sum, item) => sum + item, 0).toFixed(2));
 };
+/*Rick wants a faster way to get the product of the largest pair in an array. Your task is to create a performant solution to find the product of the largest two integers in a unique array of positive numbers.
+All inputs will be valid.
+Passing [2, 6, 3] should return 18, the product of [6, 3].
+
+Disclaimer: only accepts solutions that are faster than his, which has a running time O(nlogn).
+
+maxProduct([2, 1, 5, 0, 4, 3])              // 20
+maxProduct([7, 8, 9])                       // 72
+maxProduct([33, 231, 454, 11, 9, 99, 57])   // 104874*/
+function maxProduct(a) {
+  a.sort((a, b) => b - a);
+  return a[0] * a[1];
+}
