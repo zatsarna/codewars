14435115b1d21376030b57f0c66558d5a64af3b1
zatsarna <lyudazats@gmail.com>
2023-06-02 12:14:31 -0400

    reduce method

--- script.js
+++ script.js
@@ -254,3 +254,19 @@ function friend(friends) {
   return friends.filter((el) => el.length === 4);
 }
 console.log(friend(["Ryan", "Kieran", "Mark"]));
+/*
+Complete the function that takes a list of numbers (nums), as the only argument to the function. Take each number in the list and square it if it is even, or square root the number if it is odd. Take this new list and return the sum of it, rounded to two decimal places.
+
+The list will never be empty and will only contain values that are greater than or equal to zero.
+*/
+const sumSquareEvenRootOdd = (ns) => {
+  let newArr = [];
+  for (let i = 0; i < ns.length; i++) {
+    if (ns[i] % 2 === 0) {
+      newArr.push(ns[i] ** 2);
+    } else {
+      newArr.push(Math.sqrt(ns[i]));
+    }
+  }
+  return Number(newArr.reduce((sum, item) => sum + item, 0).toFixed(2));
+};
