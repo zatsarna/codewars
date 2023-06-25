c8ca877e5734228a7b5ea0269db3cb7d628b1f15
zatsarna <lyudazats@gmail.com>
2023-06-24 21:02:41 -0400

    recursion

--- script.js
+++ script.js
@@ -360,16 +360,24 @@ function findOdd(A) {
   }
 }
 
-console.log(findOdd([1, 1, 2]));
-console.log("------");
+/*Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
 
-const counter = () => {
-  let count = 0;
-  return () => {
-    console.log(count++);
-  };
-};
-const count1 = counter(); //  () => {console.log(count++)  };
-count1();
-count1();
-count1();
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2*/
+
+function digitalRoot(n) {
+  let arr = String(n)
+    .split("")
+    .reduce((sum, i) => sum + Number(i), 0);
+
+  if (arr > 9) {
+    return digitalRoot(arr);
+  } else {
+    return arr;
+  }
+}
+
+console.log(digitalRoot(942));
