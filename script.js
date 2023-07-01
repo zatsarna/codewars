7ba269bff9efc4ffbd6e73379bd0f4fff4f11745
zatsarna <lyudazats@gmail.com>
2023-07-01 19:30:17 -0400

    array methods

--- script.js
+++ script.js
@@ -380,4 +380,22 @@ function digitalRoot(n) {
   }
 }
 
-console.log(digitalRoot(942));
+/*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]*/
+
+function arrayDiff(a, b) {
+  let newArr = [];
+  for (let i = 0; i < a.length; i++) {
+    if (!b.includes(a[i])) {
+      newArr.push(a[i]);
+    }
+  }
+  return newArr;
+}
+console.log(arrayDiff([1, 2, 2], [1]));
