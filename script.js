6e27ef0073bcbb8dbd65fba9f55f967d5b18ca49
zatsarna <lyudazats@gmail.com>
2023-07-17 19:56:58 -0400

    array methods

--- script.js
+++ script.js
@@ -512,3 +512,10 @@ function duplicateCount(text) {
   }
   return count;
 }
+/*Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.*/
+function spinWords(string) {
+  return string
+    .split(" ")
+    .map((el) => (el.length >= 5 ? el.split("").reverse().join("") : el))
+    .join(" ");
+}
