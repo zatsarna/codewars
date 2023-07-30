35319d1081d88ed455814e5bcb487ec403ccb658
zatsarna <lyudazats@gmail.com>
2023-07-29 22:07:08 -0400

    kata6

--- script.js
+++ script.js
@@ -534,3 +534,51 @@ function findOutlier(integers) {
   const even = integers.filter((el) => el % 2 === 0);
   return odd.length === 1 ? odd[0] : even[0];
 }
+
+/*In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )*/
+
+function alphabetPosition(text) {
+  let alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  return text
+    .toUpperCase()
+    .split("")
+    .filter((el) => alphabet.includes(el))
+    .map((el) => alphabet.indexOf(el) + 1)
+    .join(" ");
+}
+console.log(alphabetPosition("E ab*"));
